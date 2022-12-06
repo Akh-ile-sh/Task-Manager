202d66d77e1bd9fae76b75a362915f5e0fce18a7
Akhilesh C <akhileshcvdy@gmail.com>
2022-12-07 01:06:39 +0530

    connected to mongoDB and set up dotenv

--- starter/app.js
+++ starter/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
 app.use(express.json());
 
 app.get("/hello", (req, res) => {
@@ -11,6 +14,15 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`server is listening at port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server is listening at port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
